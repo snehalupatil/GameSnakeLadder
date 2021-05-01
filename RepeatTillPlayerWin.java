80d4f8f21af3d6c18c224353acb54c0060170620
snehalupatil <patilsnehal4575@gmail.com>
2021-05-01 12:00:33 +0530

    Added: Repeat till the player reaches to 100

---
+++ RepeatTillPlayerWin.java
@@ -0,0 +1,40 @@
+
+public class RepeatTillPlayerWin {
+
+	public static void main(String[] args) {
+		int position=0;
+        int dieNumber,option;
+        int newPosition=1;
+        int previousPosition = 0;
+        
+        while(newPosition<=100) {
+            position = (int) ((Math.random() * 6) + 1);
+            option=(int)((Math.random()*3)+1);
+            switch (option){
+                case 1:
+                    //ladder
+                    previousPosition=newPosition;
+                    newPosition=newPosition+position;
+                    System.out.println(newPosition);
+
+                    if(newPosition<0){
+                      newPosition=previousPosition;
+                  }
+                    
+                    break;
+                case 2:
+                    //snake
+                    if (previousPosition>newPosition){
+                        newPosition=newPosition-position;
+                            
+                    }
+                    break;
+                default:
+                    newPosition=newPosition;
+                   
+                    //stay as it is
+	}
+
+}
+}
+}
